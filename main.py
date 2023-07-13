37994ec1ccc6b429cea660036e5616cf1c207609
JAAR <joe.aaron.acosta@outlook.com>
2023-07-12 20:27:28 -0400

    Version 0

---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+>>> JAAR
+>>> 07/12/2023
+>>> Practicing Fundamentals Program 4
+>>> Version 0
+'''
+
+'''
+>>> Generate a program that will take a year and will determine if the year inputted was a leap year or not.
+'''
+
+def main() :
+    print('Hello World!')
+
+
+if __name__ == '__main__' :
+    main()
